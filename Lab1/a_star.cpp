1632cae653a9c657244d8263f7d235f0d90a24d8
Simon Brefält <simbr615@student.liu.se>
2021-04-07 13:19:58 +0200

    Improved datastructure for closed moves

--- Lab1/a_star.cpp
+++ Lab1/a_star.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <utility>
 #include <chrono>
+#include <functional>
+#include <unordered_set>
 
 /*
 Indices in puzzle:
@@ -20,18 +22,19 @@ enum Move
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT,
+    NONE
 };
 
 const std::array<std::vector<Move>, 9> LEGAL_MOVES = {
     std::vector<Move>{Move::DOWN, Move::RIGHT},                       // 0, 0
     std::vector<Move>{Move::LEFT, Move::DOWN, Move::RIGHT},           // 0, 1
     std::vector<Move>{Move::DOWN, Move::LEFT},                        // 0, 2
-    std::vector<Move>{Move::UP, Move::DOWN, Move::LEFT},              // 1, 0
+    std::vector<Move>{Move::UP, Move::DOWN, Move::RIGHT},             // 1, 0
     std::vector<Move>{Move::UP, Move::DOWN, Move::LEFT, Move::RIGHT}, // 1, 1
     std::vector<Move>{Move::UP, Move::DOWN, Move::LEFT},              // 1, 2
     std::vector<Move>{Move::UP, Move::RIGHT},                         // 2, 0
-    std::vector<Move>{Move::LEFT, Move::RIGHT, Move::RIGHT},          // 2, 1
+    std::vector<Move>{Move::LEFT, Move::UP, Move::RIGHT},             // 2, 1
     std::vector<Move>{Move::UP, Move::LEFT}                           // 2, 2
 };
 
@@ -51,6 +54,8 @@ Puzzle make_move(const Move &m, Puzzle p)
         break;
     case Move::RIGHT:
         std::swap(*empty_slot, *(empty_slot + 1));
+    case Move::NONE:
+        break;
     default:
         break;
     }
@@ -62,25 +67,58 @@ struct Node
     Puzzle p;
     int cost;
     
+    // Link back to previous node to find the solution path
+    std::shared_ptr<Node> parent = nullptr;
+
+    // Save the Move that led to this Node
+    Move m = Move::NONE;
+
     // Nodes are equal if their puzzle configuration are equal
-    bool operator== (const Node &other) {
+    bool operator== (const Node &other) const {
         return std::equal(p.begin(), p.end(), other.p.begin());
     }
 };
 
+// Implement hash function for Node
+namespace std {
+    template<>
+    struct hash<Node>
+    {
+        std::size_t operator()(const Node &n) const
+        {
+            unsigned int polynomial = 0;
+            for(int i = 0; i < 9; ++i)
+            {
+                polynomial += (unsigned int)std::pow(n.p[i], i);
+            }
+
+            return std::size_t(std::hash<unsigned int>()(
+                polynomial
+            ));
+        }
+    };
+}
+
+// {4, 1, 3, 7, 2, 6, 0, 5, 8}    ##### easy with 6 moves
+// {7, 2, 4, 5, 0, 6, 8, 3, 1}     ##### medium 20 moves
+// {6, 4, 7, 8, 5, 0, 3, 2, 1}    ##### difficult 31 moves
+
+// 31 moves (most difficult) {8, 6, 7, 2, 5, 4, 3, 0, 1};
+
+
 Puzzle create_new_puzzle(int num_shuffle_moves = 100)
 {
-    Puzzle p = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+    Puzzle p = {6, 4, 7, 8, 5, 0, 3, 2, 1};
 
-    for (int i = 0; i < num_shuffle_moves; ++i) {
-        auto empty_slot = std::find(p.begin(), p.end(), 0);
-        int index = empty_slot - p.begin();
-        auto legal_moves = LEGAL_MOVES[index];
+    // for (int i = 0; i < num_shuffle_moves; ++i) {
+    //     auto empty_slot = std::find(p.begin(), p.end(), 0);
+    //     int index = empty_slot - p.begin();
+    //     auto legal_moves = LEGAL_MOVES[index];
 
-        // Select a random move to make
-        int selection = rand() % legal_moves.size();
-        p = make_move(legal_moves[selection], p);
-    }
+    //     // Select a random move to make
+    //     int selection = rand() % legal_moves.size();
+    //     p = make_move(legal_moves[selection], p);
+    // }
 
     // Old, possibly degenerate solution for shuffling
     // std::random_shuffle(p.begin(), p.end());
@@ -112,6 +150,16 @@ int cells_out_of_place(const Puzzle &p, const Puzzle &s)
     return count;
 }
 
+
+// int manhattan_distance(const Puzzle &p, const Puzzle &s)
+// {
+//     int total = std::reduce(
+//         p.begin(), p.end(),
+        
+//     );
+//     return total;
+// }
+
 std::vector<Node> expand_node(const Node &node)
 {
     // Maybe there should be a struct holding empty_slot index/iterator
@@ -123,15 +171,14 @@ std::vector<Node> expand_node(const Node &node)
 
     std::transform(legal_moves.begin(), legal_moves.end(),
                    std::back_inserter(result),
-                   [&node](const Move &m) { return Node{make_move(m, node.p), node.cost + 1}; });
+                   [&node](Move m) { return Node{make_move(m, node.p), node.cost + 1, std::make_shared<Node>(node), m}; });
     return result;
 }
 
-void solve_puzzle(const Puzzle &p, const Puzzle &s, Heuristic heuristic)
+std::vector<Puzzle> solve_puzzle(const Puzzle &p, const Puzzle &s, Heuristic heuristic)
 {
     std::vector<Node> open;
-    std::vector<Node> closed;
-    std::vector<Move> moves;
+    std::unordered_set<Node> closed;
 
     // Rank nodes by lowest estimated cost
     auto compare_nodes = [&s, heuristic](const Node &a, const Node &b) -> bool {
@@ -147,8 +194,8 @@ void solve_puzzle(const Puzzle &p, const Puzzle &s, Heuristic heuristic)
         current = open.front())                             // Update current node
     {
         // Print current puzzle state for debugging
-        print_puzzle(current.p);
-        std::cout << '\n';
+        // print_puzzle(current.p);
+        // std::cout << '\n';
 
         // Expand current node
         std::vector<Node> children = expand_node(current);
@@ -156,18 +203,26 @@ void solve_puzzle(const Puzzle &p, const Puzzle &s, Heuristic heuristic)
             children.begin(),
             children.end(),
             std::back_inserter(open),
-            [&closed](const Node &n) { return std::find(closed.begin(), closed.end(), n) == closed.end(); });
+            [&closed](const Node &n) { return closed.find(n) == closed.end(); });
 
         // Make sure the current node is moved to the closed list
-        closed.emplace_back(current);
+        closed.emplace(current);
         // Re-heapify and remove last element
-        std::pop_heap(open.begin(), open.end(), compare_nodes);
+        // std::pop_heap(open.begin(), open.end(), compare_nodes);
+        std::swap(open.front(), open.back());
         open.pop_back();
         std::make_heap(open.begin(), open.end(), compare_nodes);
     }
     
     // The node corresponding to the solution is now on top of the heap
-    print_puzzle(open.front().p);
+    std::vector<Puzzle> moves;
+    moves.push_back(open.front().p);
+    for(auto n = open.front().parent; n != nullptr; n = n->parent)
+    {
+        moves.push_back(n->p);
+    }
+
+    return moves;
 }
 
 int main()
@@ -183,18 +238,24 @@ int main()
     // The random puzzle to solve
     Puzzle puzzle = create_new_puzzle();
     std::cout << "Puzzle\n";
-    // print_puzzle(puzzle);
+    print_puzzle(puzzle);
     // std::cout << '\n';
+
+    // Sanity checks
+    std::cout << "\nInitial number of cells out of place: " << cells_out_of_place(puzzle, solved) << '\n';
 
     // Solve the puzzle
     auto start = std::chrono::high_resolution_clock::now();
-    solve_puzzle(puzzle, solved, cells_out_of_place);
+    std::vector<Puzzle> road_to_victory = solve_puzzle(puzzle, solved, cells_out_of_place);
     auto end = std::chrono::high_resolution_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
     std::cout << "\nTook " << time.count()/1000.0f << " seconds to solve\n";
 
-    // Sanity checks
-    std::cout << "Initial number of cells out of place: " << cells_out_of_place(puzzle, solved) << '\n';
+    // Print the solution
+    std::cout << std::endl << "The steps taken: " << std::endl;
+    std::for_each(road_to_victory.rbegin(), road_to_victory.rend(),  [](const Puzzle &p){ print_puzzle(p); std::cout << '\n'; });
+
+
 
     // // Puzzle dummy = {1, 1, 1, 1, 0, 1, 1, 1, 1}; // Simple test puzzle
     // // std::cout << "Move up:\n";
